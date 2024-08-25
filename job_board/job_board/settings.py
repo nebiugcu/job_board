30324b62727c7fe9a8217d0051c392d27812b1ba
netnaho <87896136+netnaho@users.noreply.github.com>
2024-08-25 21:35:13 +0300

    some changes

--- job_board/job_board/settings.py
+++ job_board/job_board/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'job_board',
         'USER': 'postgres',
-        'PASSWORD': 'dawit@03',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432',
     }
