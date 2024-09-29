191b93ee49fd85c06cbaae612bf71faf77356dc3
nebiugcu <146277454+nebiugcu@users.noreply.github.com>
2024-09-29 15:06:07 -0700

    db-change

--- job_board/job_board/settings.py
+++ job_board/job_board/settings.py
@@ -112,9 +112,9 @@ WSGI_APPLICATION = 'job_board.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'job_board'),
+        'NAME': os.getenv('DB_NAME', 'job_board2'),
         'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', '12345'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'dawit@03'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
