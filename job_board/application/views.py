3ba16d1099b7f4c1d97ff3baebc8e310762e2688
netnaho <87896136+netnaho@users.noreply.github.com>
2024-10-08 21:12:21 +0300

    minor changes

--- job_board/application/views.py
+++ job_board/application/views.py
@@ -154,3 +154,52 @@ def has_applied(request, job_id):
         return Response({"has_applied": True}, status=200)
     else:
         return Response({"has_applied": False}, status=200)
+
+
+# candidate match
+
+def candidate_match_view(request):
+    if request.method == 'POST':
+        form = CandidateMatchForm(request.POST)
+        if form.is_valid():
+            job_title = form.cleaned_data['job_title']
+            location = form.cleaned_data['location']
+            required_skills = form.cleaned_data['skills'].split(',')
+            experience_level = form.cleaned_data['experience_level']
+
+            # Fetch applications for jobs that match the given job title and location
+            applications = Application.objects.filter(
+                job__job_title__icontains=job_title,
+            )
+
+            # Calculate match scores for candidates based on job_seeker skills
+            matched_candidates = []
+            for application in applications:
+                job_seeker = application.job_seeker
+
+                if job_seeker.skills:  # Ensure job seeker has listed skills
+                    candidate_skills = job_seeker.skills.split(',')
+
+                    # Calculate skills match using TF-IDF and cosine similarity
+                    vectorizer = TfidfVectorizer()
+                    skills_vecs = vectorizer.fit_transform([', '.join(required_skills), ', '.join(candidate_skills)])
+                    cosine_sim = cosine_similarity(skills_vecs[0], skills_vecs[1]).flatten()[0]
+
+                    match_score = cosine_sim * 100
+
+                    # Add job seeker details with match score
+                    matched_candidates.append({
+                        'first_name': job_seeker.user.first_name,
+                        'last_name': job_seeker.user.last_name,
+                        'profession': job_seeker.profession,
+                        'score': match_score
+                    })
+
+            # Sort candidates by match score in descending order and limit to top 5
+            matched_candidates = sorted(matched_candidates, key=lambda x: x['score'], reverse=True)[:5]
+
+            return render(request, 'employers/matched_candidates.html', {'matched_candidates': matched_candidates})
+    else:
+        form = CandidateMatchForm()
+
+    return render(request, 'employers/candidate_match_form.html', {'form': form})
